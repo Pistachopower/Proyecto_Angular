ca2d714a53db27e9fc058f552c3fcc37a4f9ac75
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2025-01-29 12:27:09 +0000

    Inicio Angular

---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Empleado } from './empleado';
+
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html',
+  styleUrls: ['./empleado.component.css']
+})
+
+export class EmpleadoComponent {
+  public titulo = 'Bienvenido';
+  public empleado: Empleado;
+  public trabajador: Empleado[];
+  public trabajadoresExterno: boolean;
+
+  constructor() {
+    this.empleado = new Empleado('Pedro', 30, "Gerente", true);
+
+    this.trabajador = [
+      new Empleado('Manolo', 35, "Administrativo", true),
+      new Empleado('Ana', 25, "Programador", true),
+      new Empleado('Maria', 28, "Ingeniera", false)
+    ];
+
+    this.trabajadoresExterno = true;
+  }
+}
